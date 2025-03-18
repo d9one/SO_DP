15a08e84bedfd541ff2ca7761af8a842fd0408ee
jakub <273124@student.pwr.edu.pl>
2025-03-18 18:58:26 +0100

    console output update

--- main.cpp
+++ main.cpp
@@ -1,16 +1,30 @@
 #include <complex>
 #include <iostream>
+#include <mutex>
 #include <unistd.h>
 #include "pthread.h"
 #include "semaphore.h"
 
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define RESET "\033[0m"
+
 sem_t* forks; // semaphores for each fork
 sem_t table; // semaphore for the table
 int n; // number of philosophers
+bool* isEating; // array to keep track of which philosophers are eating
+pthread_mutex_t printMutex; // mutex to synchronize printing
 
 struct PhilosopherData {
     int id;
 };
+
+void initialize_isEating(int n) { // initialize the isEating array
+    isEating = new bool[n];
+    for (int i = 0; i < n; i++) {
+        isEating[i] = false;
+    }
+}
 
 void initialize_forks(int n) { // initialize the semaphores
     forks = new sem_t[n];
@@ -26,19 +40,40 @@ void free_forks(int n) { // free the semaphores
     delete[] forks;
     sem_destroy(&table);
 }
+
+void printPhilosophers(int n) { // print the state of each philosopher
+    pthread_mutex_lock(&printMutex);
+    system("clear");
+
+    std::cout << "+----+-------------------------+\n";
+    for (int i = 0; i < n; i++) {
+        std::cout << "| " << i + 1 << "  | ";
+        if (isEating[i])
+            std::cout << RED << "Philosopher is EATING  " << RESET;
+        else
+            std::cout << GREEN << "Philosopher is THINKING" << RESET;
+        std::cout << " |\n";
+    }
+    std::cout << "+----+-------------------------+\n";
+    pthread_mutex_unlock(&printMutex);
+}
+
+
 void* create_philosopher(void* arg) { // create a philosopher
     auto* data = (PhilosopherData*)arg;
     int id = data->id;
 
     while (true) {
-        std::cout << "Philosopher " << id << " is thinking" << std::endl;
+        printPhilosophers(n);
         sleep(10);
 
         // get the forks and table
         sem_wait(&table);
         sem_wait(&forks[id]);
         sem_wait(&forks[(id + 1) % n]);
-        std::cout << "Philosopher " << id << " is eating" << std::endl;
+
+        isEating[id] = true;
+        printPhilosophers(n);
         sleep(5);
 
         // free the forks and table
@@ -46,6 +81,7 @@ void* create_philosopher(void* arg) { // create a philosopher
         sem_post(&forks[(id + 1) % n]);
         sem_post(&table);
 
+        isEating[id] = false;
     }
 }
 int main(int argc, char* argv[]) {
@@ -59,6 +95,8 @@ int main(int argc, char* argv[]) {
         return 1;
     }
     initialize_forks(n);
+    initialize_isEating(n);
+    pthread_mutex_init(&printMutex, NULL);
     auto* threads = new pthread_t[n];
     for (int i = 0; i < n; i++) {
         auto* data = new PhilosopherData{i};
@@ -71,5 +109,7 @@ int main(int argc, char* argv[]) {
 
     free_forks(n);
     delete[] threads;
+    delete[] isEating;
+    pthread_mutex_destroy(&printMutex);
     return 0;
 }
